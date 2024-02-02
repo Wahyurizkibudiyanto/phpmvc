317e35e02d04d19952615639b57a7fe2e165309e
Wahyu Rizki Budiyanto <wahyudasilfa@gmail.com>
2024-02-02 18:57:23 +0700

    latihan 2

--- app/views/about/index.php
+++ app/views/about/index.php
@@ -1,6 +1,6 @@
-<div class="container">
-    <img src="<?= BASEURL; ?>/img/wahyu.jpg" alt="wahyu rizki">
-    <h1>About Me</h1>
+<div class="container mt-5">
+    <img src="<?= BASEURL; ?>/img/wahyu.jpg" alt="wahyu rizki" class="rounded shadow">
+    <h1 class="mt-4">About Me</h1>
     <p>Halo, Nama saya <?= $data['nama']; ?>, umur saya <?= $data['umur']; ?>, pekerjaan <?= $data['pekerjaan']; ?></p>
 
 </div>
