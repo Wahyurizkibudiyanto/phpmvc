8820edc3aa1104489283e343fc83780fa10616ba
Wahyu Rizki Budiyanto <wahyudasilfa@gmail.com>
2024-02-12 19:54:33 +0700

    latihan databases

--- app/core/Database.php
+++ app/core/Database.php
@@ -16,8 +16,9 @@ class Database
         $dsn = 'mysql:host=' . $this->host . ';dbname=' . $this->db_name; //identitas server
 
         $option = [
-            PDO::ATTR_PERSISTENT
-        ]
+            PDO::ATTR_PERSISTENT => true,
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+        ];
 
         try { //pengecekan koneksi ke databases
             $this->dbh = new PDO($dsn, $this->user, $this->pass, $option);
@@ -26,3 +27,9 @@ class Database
         }
     }
 }
+
+public function query ($query) {
+    $this->stmt = $this->dbh->prepare->($query);
+}
+
+public function bind ($param, $value, $type = null);
